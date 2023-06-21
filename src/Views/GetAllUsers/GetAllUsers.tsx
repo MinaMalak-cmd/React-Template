a832d5d79594879895f2c6bb2b2c922253d69329
MinaMalak-cmd <engmina15@yahoo.com>
2023-06-21 15:25:11 +0300

    retrieve users

--- src/Views/GetAllUsers/GetAllUsers.tsx
+++ src/Views/GetAllUsers/GetAllUsers.tsx
@@ -3,24 +3,12 @@ import MyButton from "../../Components/MyButton/MyButton";
 import useGetAllUsers from "./useGetAllUsers";
 
 const GetAllUsers = () => {
-  const { x } = useGetAllUsers();
+  const { users } = useGetAllUsers();
   return (
     <>
       <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0">
           <Accordion.Header>Accordion Item #1</Accordion.Header>
-          <Accordion.Body>
-            {x}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
           <Accordion.Body>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -31,6 +19,16 @@ const GetAllUsers = () => {
             culpa qui officia deserunt mollit anim id est laborum.
           </Accordion.Body>
         </Accordion.Item>
+        {users.map((user:any) =>{
+          return (
+            <Accordion.Item eventKey={user.id}>
+              <Accordion.Header>{user.name}</Accordion.Header>
+              <Accordion.Body>
+                {user.email}
+              </Accordion.Body>
+            </Accordion.Item>
+          )
+        })}
       </Accordion>
       <MyButton />
     </>
--- src/Views/GetAllUsers/useGetAllUsers.ts
+++ src/Views/GetAllUsers/useGetAllUsers.ts
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+import { get } from "../../Services/httpMethods";
 
 const useGetAllUsers = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const getUsers = async() =>{
+        const response = await get("/user");
+        setUsers(response?.data.result);
+    }
     return {
-        x : 5,
+        users
     };
 }
  
