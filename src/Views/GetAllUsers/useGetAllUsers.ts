8b94e4bc24678c2332d74604e438b41ef4fe5e7f
MinaMalak-cmd <engmina15@yahoo.com>
2023-06-21 15:09:30 +0300

    add axios methods

--- src/Views/GetAllUsers/useGetAllUsers.ts
+++ src/Views/GetAllUsers/useGetAllUsers.ts
@@ -1,7 +1,5 @@
-import { get } from "../../Services/axiosConfig";
 
 const useGetAllUsers = () => {
-    // console.log("getAllUsers", get('https://jsonplaceholder.typicode.com/posts/1'))
     return {
         x : 5,
     };
