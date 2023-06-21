9c4292cc7b5a7ac00c9958dfff701de33723f949
MinaMalak-cmd <engmina15@yahoo.com>
2023-06-21 16:26:45 +0300

    start in add and update

--- src/Views/GetAllUsers/GetAllUsers.tsx
+++ src/Views/GetAllUsers/GetAllUsers.tsx
@@ -1,8 +1,23 @@
+import { loadFull } from "tsparticles"; 
+import Particles from "react-tsparticles"; 
+
+import particles from "../../Assets/animations/particles.json";
 import useGetAllUsers from "./useGetAllUsers";
+import {ButtonGroup} from "react-bootstrap";
 
 const GetAllUsers = () => {
-  const { users } = useGetAllUsers();
+  const { users, setDeletedItem, deletedItem, deleteUser, setUpdatedItem, updatedItem } = useGetAllUsers();
   return (
+    <>
+       {/* <Particles 
+        id="tsparticles" 
+        // init={particlesInit} 
+        // loaded={particlesLoaded} 
+        particles={particles}
+        options={{ 
+          fpsLimit: 60, 
+        }} 
+      />  */}
       <div className="container-fluid py-3 px-3">
         <div className="row">
           <div className="col-sm-12">
@@ -13,6 +28,7 @@ const GetAllUsers = () => {
                   <th>Name</th>
                   <th>Email</th>
                   <th>Age</th>
+                  <th>Actions</th>
                 </tr>
               </thead>
               <tbody>
@@ -23,6 +39,12 @@ const GetAllUsers = () => {
                       <td>{user.name}</td>
                       <td>{user.email}</td>
                       <td>{user.age}</td>
+                      <td>
+                      <div aria-label="user table actions" className="d-flex">
+                        <button className="btn btn-primary" onClick={() => setUpdatedItem(user)}>Update</button>
+                        <button className="btn btn-danger mx-2" onClick={() => setDeletedItem(user.id)}>Delete</button>
+                      </div>
+                      </td>
                     </tr>
                   );
                 })}
@@ -31,6 +53,7 @@ const GetAllUsers = () => {
           </div>
         </div>
       </div>
+    </>
   );
 };
 
--- src/Views/GetAllUsers/useGetAllUsers.ts
+++ src/Views/GetAllUsers/useGetAllUsers.ts
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
-import { get } from "../../Services/httpMethods";
+import { deleteRequest, get } from "../../Services/httpMethods";
 
 const useGetAllUsers = () => {
     const [users, setUsers] = useState([]);
+    const [deletedItem, setDeletedItem] = useState(NaN);
+    const [updatedItem, setUpdatedItem] = useState<any>();
+    console.log("🚀 ~ file: useGetAllUsers.ts:8 ~ useGetAllUsers ~ updatedItem:", updatedItem, deletedItem)
     useEffect(() => {
         getUsers();
     }, []);
@@ -11,8 +14,19 @@ const useGetAllUsers = () => {
         const response = await get("/user");
         setUsers(response?.data.result);
     }
+    const deleteUser = async() =>{
+        // const response = await deleteRequest(`/user/${deletedItem}`);
+        console.log("🚀 ~ file: useGetAllUsers.ts:18 ~ deleteUser ~ deletedItem:", deletedItem);
+        setDeletedItem(NaN);
+        getUsers();
+    }
     return {
-        users
+        users,
+        setDeletedItem,
+        deletedItem,
+        deleteUser,
+        setUpdatedItem,
+        updatedItem
     };
 }
  
